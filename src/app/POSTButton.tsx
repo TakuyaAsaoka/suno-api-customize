75630de17d9fb059b6a419701b76ad59015b0d5c
Ogata-Kazuyoshi <kazuyoshi_ogata_ab@mail.toyota.co.jp>
2024-06-28 01:37:22 +0900

    とりあえず曲をクリックできるようになりました

--- src/app/POSTButton.tsx
+++ src/app/POSTButton.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import axios, {AxiosResponse} from "axios";
 
 export function POSTButton() {
@@ -18,19 +18,36 @@ export function POSTButton() {
     console.log({res})
     const musicId = res.data[0].id
     console.log({musicId})
-    const getMusicFromId:any[] = await axios.get(`/api/get?id=${musicId}`,{withCredentials:true})
-    const filteredMusic = getMusicFromId.filter(music => music.audio_url !== "")
-    const urls = filteredMusic.map(elm => elm.audio_url)
-    console.log({filteredMusic})
-    setMusic(urls)
+    setTimeout(async ()=>{
+      const getMusicFromId:any[] = await axios.get(`/api/get?id=${musicId}`,{withCredentials:true}).then(res => res.data)
+      console.log({getMusicFromId})
+      console.log(Array.isArray(getMusicFromId))
+      const filteredMusic = getMusicFromId.filter(music => music.audio_url !== "")
+      const urls = filteredMusic.map(elm => elm.audio_url)
+      console.log({filteredMusic})
+      setMusic(urls)
+    },60000)
   }
 
   const handleOnClick = async () => {
     await generateMusic()
   }
 
+  const handleNoPostClick = async () => {
+    const getMusicFromId:any[] = await axios.get(`/api/get`).then(res => res.data)
+    // console.log({getMusicFromId})
+    // console.log(Array.isArray(getMusicFromId))
+    const filteredMusic = getMusicFromId.filter(music => music.audio_url !== "")
+    const urls = filteredMusic.map(elm => elm.audio_url)
+    // console.log({filteredMusic})
+    setMusic(urls)
+  }
+
+
   return (
     <div>
+      <button onClick={handleNoPostClick}>GETオンリー</button>
+      <br/>
       <button onClick={handleOnClick}>POST</button>
       <label>
         prompt:
@@ -39,8 +56,19 @@ export function POSTButton() {
         }}></textarea>
       </label>
       {music.map((m, index) => {
-        <a key={index} href={m} rel="noopener noreferrer" target="_blank">Music</a>
+        return <a key={index} href={m} rel="noopener noreferrer" target="_blank">Music</a>
       })}
     </div>
   )
 }
+
+const idList = [
+    "6bdd0814-433f-43e4-9849-3f387b5dfae1",
+    "57a5dfd2-ea58-44c6-9a2c-5b99f0aa7cf2",
+    "191d62e4-2a40-4cfc-9f02-e0e1a7d8f182",
+    "d2b02967-fb0b-4a07-a080-2d2af8972b31",
+  "4e806161-aa45-41e4-913d-2467b3a1cba9",
+    "5c2c85fe-041c-4e37-9109-388a6ba3f57e",
+    "f00484b6-f932-4748-9ba2-2cc86943d914",
+    "fba6aced-ba3e-4aaf-b28e-93a448bb5004"
+]
